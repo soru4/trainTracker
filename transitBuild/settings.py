89f047c2904bd9353d5105df3528702f799d7ca0
soru4 <73024932+soru4@users.noreply.github.com>
2025-11-09 11:48:13 -0600

    Fix Django Settings

--- transitBuild/settings.py
+++ transitBuild/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-----'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
